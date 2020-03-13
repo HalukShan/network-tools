9f7bf8ab07c08d86e421ce22b5794399c730e23b
Haluk Shan <haluk@HalukdeMacBook-Pro.local>
2020-03-13 21:34:17 +0800

    init

---
+++ FIN-scanner.py
@@ -0,0 +1,32 @@
+import argparse
+from scapy.layers.inet import TCP, IP, ICMP
+from scapy.all import *
+
+
+def scan():
+    open = []
+    closed = []
+    for port in dst_port:
+        rev = sr1(IP(dst=dst_ip) / TCP(sport=src_port, dport=dst_port, flags="F"), timeout=5)
+        if not rev:
+            open.append(str(port) + "    Probable Open")
+        elif rev.haslayer(TCP):
+            if rev.getlayer(TCP).flags == "R":
+                closed.append(str(port) + "    Closed")
+    print("     ", dst_ip)
+    for o in open:
+        print(o)
+    for c in closed:
+        print(c)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dst_ip", help="Destination ip address", type=str)
+    parser.add_argument("dst_port", help="Display a cubic of a given number", nargs='+', type=int)
+    args = parser.parse_args()
+    dst_ip = args.dst_ip
+    dst_port = args.dst_port
+    src_port = RandShort()
+    print(dst_port)
+    scan()
---
+++ SYN-scanner.py
@@ -0,0 +1,43 @@
+import argparse
+from scapy.layers.inet import TCP, IP, ICMP
+from scapy.all import *
+
+
+def scan():
+    open = []
+    filtered = []
+    closed = []
+    for port in dst_port:
+        rev = sr1(IP(dst=dst_ip) / TCP(sport=src_port, dport=port, flags="S"), timeout=10)
+        if not rev:
+            filtered.append(str(port) + "    Filtered")
+        elif rev.haslayer(TCP):
+            if rev.getlayer(TCP).flags == "SA":
+                send(IP(dst=dst_ip) / TCP(sport=src_port, dport=dst_port, flags="R"))
+                open.append(str(port) + "    Open")
+            elif rev.getlayer(TCP).flags == "R":
+                closed.append(str(port) + "    Closed")
+        # Intercepted !
+        elif rev.haslayer(ICMP):
+            if int(rev.getlayer(ICMP).type) == 3 and int(rev.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]:
+                print("Filtered")
+                filtered.append(rev)
+    print("     ", dst_ip)
+    for o in open:
+        print(o)
+    for f in filtered:
+        print(f)
+    for c in closed:
+        print(c)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dst_ip", help="Destination ip address", type=str)
+    parser.add_argument("dst_port", help="Display a cubic of a given number", nargs='+', type=int)
+    args = parser.parse_args()
+    dst_ip = args.dst_ip
+    dst_port = args.dst_port
+    src_port = RandShort()
+    print(dst_port)
+    scan()
